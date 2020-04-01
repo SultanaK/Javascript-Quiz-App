a9541442ef4de8a1690e17c43a8a37cad29974ce
Your name here <kr.shampa@yahoo.com>
2020-04-01 16:17:28 -0500

    adding application

---
+++ JS/store.js
@@ -0,0 +1,133 @@
+const myQuestions = [
+    {
+        question: "What is a string in JavaScript？",
+        answers: {
+            a: "string that you can tie your notes to",
+            b: "a string that you can pluck and create musical sound",
+            c: "JavaScript strings are used for storing and manipulating text",
+            d: "All of the above"
+        },
+        correctAnswer: "c"
+    },
+    {
+        question: "Inside which HTML element do we put the JavaScript?",
+        answers: {
+            a: "< JavaScript>",
+            b: "< scripting>" ,
+            c: "< script>",
+            d: "< js>"
+        },
+        correctAnswer: "c"
+    },
+    {
+        question: "How do you write 'Hello World' in an alert box?",
+        answers: {
+            a: "msg('Hello World')",
+            b: "alertBox('Hello world')",
+            c: "alert('Hello World')",
+            d: "All of the above"
+        },
+        correctAnswer: "c"
+    },
+    {
+        question: "Where is the correct place to insert a JavaScript? ",
+        answers: {
+            a: "The body section",
+            b: "The head section",
+            c: "None of the above",
+            d: "All of the above"
+        },
+        correctAnswer: "d"
+    },
+    {
+        question: "How do you create a function in JavaScript?",
+        answers: {
+            a: "Function:myfunction",
+            b: "function myfunction()",
+            c: "Both",
+            d: "All of the above"
+        },
+        correctAnswer: "b"
+    },
+    {
+        question: "How do you call a function named 'myFunction'?",
+        answers: {
+            a: "call function myfunction",
+            b: "call myfunction",
+            c: "myFunction()",
+            d: "Both a and b"
+        },
+        correctAnswer: "c"
+    },
+    {
+        question: "How to write an IF statement in JavaScript?",
+        answers: {
+            a: "if i == 5 then",
+            b: "if i = 5",
+            c: "if (i === 5)",
+            d: "None of the above"
+        },
+        correctAnswer: "c"
+    },
+    {
+        question: "What are media queries?",
+        answers: {
+            a: "The javascript method that allows multimedia content to be played in an HTML document",
+            b: "An element that allows video to be properly displayed in documents that use HTML and CSS",
+            c: "A module that allows web content to be displayed more readily on different size screens",
+            d: "All of the above"
+        },
+        correctAnswer: "c"
+    },
+    {
+        question: "Why are grids valuable?",
+        answers: {
+            a: "They can handle both columns and rows for HTML documents",
+            b: "They are an less complicated way to create 2-dimensional styling for webpages",
+            c: "They allow the developer to change the layout without interfering with the source order",
+            d: "All of the above"
+        },
+        correctAnswer: "d"
+    },
+    {
+        question: "What is a function?",
+        answers: {
+            a: "A block of code designed to perform a specific task or calculate a value",
+            b: "A semantic tool that is invoked when a particular action is desired",
+            c: "An action that may be performed on an object",
+            d: "A container for a data value"
+        },
+        correctAnswer: "a"
+    },
+    {
+        question: "What is DOM manipulation?",
+        answers: {
+            a: "A Standard Object Model for HTML",
+            b: "A programming interface for HTML",
+            c: "A standard for how to get, change, add, or delete HTML elements",
+            d: "All of the above"
+        },
+        correctAnswer: "d"
+    },
+    {
+        question: "What is an event listener?",
+        answers: {
+            a: "A procedure or function in a computer program that waits for an event to occur",
+            b: "A console screen that registers program functions and errors",
+            c: "A value that you can set or get",
+            d: "An action that you can perform on an object"
+        },
+        correctAnswer: "a"
+    },
+    {
+        question: "What is spaghetti code? How can you avoid it, and why should you? (Lesson 3.2)",
+        answers: {
+            a: "Code that goes on for more than one line",
+            b: "Code with a tightly controlled structure",
+            c: "Burdensome, unwieldy code that is difficult to read and maintain",
+            d: "Well-structured classes that are easy to understand in isolation, but difficult to understand as a whole"
+        },
+        correctAnswer: "c"
+    }
+
+];
